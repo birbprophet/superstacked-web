f6f5d47a11a0c9ee50096ea01b674ccebce171d5
Benjamin Tang <benjamin.tang.hz@gmail.com>
2021-09-12 16:43:26 +0800

    added rum metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import "@/styles/globals.css";
+import { Metrics } from "@layer0/rum";
+
+new Metrics({
+  token: "7378dd9d-8534-4ecb-a3bc-ff0c58e34ccf",
+}).collect();
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
