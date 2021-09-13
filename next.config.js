ce553e2997f1dfc7b488cb507a6cfc1a4e2cd3cf
Benjamin Tang <benjamin.tang.hz@gmail.com>
2021-09-13 16:58:46 +0800

    removed bundle analyser

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 // This file was automatically added by layer0 init.
 // You should commit this file to source control.
-const { withLayer0, withServiceWorker } = require('@layer0/next/config')
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-const _preLayer0Export = withBundleAnalyzer({
-  reactStrictMode: true,
-});;
+const { withLayer0, withServiceWorker } = require("@layer0/next/config");
 
 module.exports = (phase, config) =>
   withLayer0(
@@ -16,7 +8,5 @@ module.exports = (phase, config) =>
       // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
       // the logs in the Layer0 developer console.
       layer0SourceMaps: true,
-
-      ..._preLayer0Export
     })
-  )
+  );
