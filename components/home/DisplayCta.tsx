8ad1206dcc20e79dd47b486087263a02c827b0df
Benjamin Tang <benjamin.tang.hz@gmail.com>
2021-09-15 02:22:23 +0800

    design adjustments

--- components/home/DisplayCta.tsx
+++ components/home/DisplayCta.tsx
@@ -193,27 +193,30 @@ export default function DisplayCta(props) {
         </div>
       </div>
       <div className="max-w-7xl mx-auto pb-16 px-4 sm:px-6 lg:px-8">
-        <div className="bg-gray-700 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
-          <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
-            <div className="lg:self-center">
-              <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+        <div className="bg-gray-700 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-11 lg:gap-4">
+          <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20 lg:col-span-6">
+            <div className="lg:self-center flex flex-col h-full">
+              <h2 className="text-4xl font-extrabold text-white sm:text-5xl sm:leading-none">
                 <span className="block text-primary-300">Start small.</span>
                 <span className="block">Think global scale.</span>
               </h2>
-              <p className="mt-4 text-lg leading-6 text-gray-200">
+              <p className="mt-4 lg:mt-8 text-lg leading-6 text-gray-200">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
                 malesuada in justo sed consequat. Lorem ipsum dolor sit amet,
                 consectetur adipiscing elit. Nam malesuada in justo sed
                 consequat.
               </p>
-              <Link href="/signup">
-                <a className="mt-8 bg-white border border-transparent rounded-md shadow px-6 py-3 inline-flex items-center text-base font-semibold text-gray-700 hover:text-gray-800 hover:bg-gray-100">
-                  Get Started
-                </a>
-              </Link>
+              <div className="flex-1"></div>
+              <div>
+                <Link href="/signup">
+                  <a className="mt-8 bg-white border border-transparent rounded-md shadow px-6 py-3 inline-flex items-center text-base font-semibold text-gray-700 hover:text-gray-800 hover:bg-gray-100">
+                    Get Started
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="p-12 hidden lg:grid">
+          <div className="py-16 pr-16 pl-8 hidden lg:grid lg:col-span-5">
             <DynamicSvg />
           </div>
         </div>
